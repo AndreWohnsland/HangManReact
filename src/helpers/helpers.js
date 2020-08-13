9087c286c6ab998f9aaf7f9fc8ffe946d5861891
Andre Wohnsland <Andre_Wohnsland@web.de>
2020-08-13 22:30:41 +0200

    Cleaned up code a little

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -9,9 +9,7 @@ export function checkWin(correct, wrong, word) {
   let status = 'win';
 
   word.split('').forEach((letter) => {
-    if (!correct.includes(letter)) {
-      status = '';
-    }
+    if (!correct.includes(letter)) status = '';
   });
 
   if (wrong.length === 6) status = 'lose';
